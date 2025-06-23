eb4acc04b36eddea9e8e2bff03dbce1170a943e6
Ashik M <ashikmd1845@gmail.com>
2025-06-23 14:53:06 +0530

    banner scroll

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -87,7 +87,7 @@ const Banner = () => {
   ScrollTrigger.create({
     trigger: section,
     start: "top top",
-    end: "bottom+=2000 top",
+    end: "bottom+=1000 top",
     scrub: true,
     pin: true,
     onUpdate: (self) => {
